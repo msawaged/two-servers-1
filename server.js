04bfb1afc6a067d0b9c57d901a437af199168f11
Murad Sawaged <muradsawaged@Murads-MacBook-Pro.local>
2018-08-09 19:19:51 -0600

    with two servers

---
+++ server.js
@@ -0,0 +1,52 @@
+// Require/import the HTTP module
+const http = require("http");
+
+// Define a port to listen for incoming requests
+function serverone() {
+
+const PORT = 7000;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Hey good job! Server listening on: http://localhost:" + PORT);
+});
+}
+
+function servertwo() {
+
+const PORT = 7050;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("YOU SUCK! Server listening on: http://localhost:" + PORT);
+});
+}
+
+serverone()
+servertwo()
